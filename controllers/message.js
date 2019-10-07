c446c5a338c436299c8e7072c090182384997e9e
madmonkeyaudio-js <jonathangross@Jonathans-MacBook-Pro.local>
2019-10-07 11:53:17 -0700

    updating message controller

--- controllers/message.js
+++ controllers/message.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express();
 const client = require('socket.io').listen(4000).sockets;
+const db = require('../models')
 
 //SOCKET SETUP
 
@@ -8,8 +9,20 @@ client.on('connection', (socket)=> {
     console.log('made a socket connection', socket.id)
 
     socket.on('chat', (data)=> {
+        db.Message.findOne()
+        .then()
+        .catch(err => {
+            console.log(err);
+        })
         client.sockets.emit('chat', data);
     })
+    //Emit the message
+    db.Message.find().limit(50).sort({_id:1})
+    .then()
+    .catch(err => {
+        console.log(err)
+    })
+
 })
 
 
