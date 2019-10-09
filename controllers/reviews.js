0dcaa5d7e2fc158524f2dd20e836b7de0195625d
gtole342 <gtole342@gmail.com>
2019-10-09 16:00:48 -0700

    l

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -55,6 +55,7 @@ router.put('/:reviewId', (req, res) => {
   .then(review => {
     console.log(review.clientId)
     console.log(req.user._id)
+    console.log(review.clientId === req.user._id)
     if (review.clientId === req.user._id) {
       review.title = req.body.title;
       review.content = req.body.content;
