76d3baa7bab73437a4a882a98b82d3c967e86efa
gtole342 <gtole342@gmail.com>
2019-10-09 19:06:59 -0700

    Added get all goats route

--- controllers/user.js
+++ controllers/user.js
@@ -7,14 +7,24 @@ router.get('/:userId', (req,res) => {
     _id: req.params.id
   })
   .then((user) => {
-    if (user.isGoat) {
-      
-    }
+    res.send({user})
   })
   .catch((err) => {
     console.log(`Error in GET/user/${req.params.id}`, err)
     res.status(503).send({ message: 'Something went wrong.' })
   })
+})
+
+router.get('/goats', (req,res) => {
+  db.User.find({
+    where: {
+      isGoat: true
+    }
+  })
+  .then((users) => {
+    res.send({users})
+  })
+  .catch()
 })
 
 module.exports = router;
